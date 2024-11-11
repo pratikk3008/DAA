47015e9f29788b0dcb4f4d4b30b5ec85e46a456b
pratikk3008 <148140944+pratikk3008@users.noreply.github.com>
2024-11-12 00:22:56 +0530

    Update A5.py

--- A5.py
+++ A5.py
@@ -22,4 +22,27 @@ if __name__ == '__main__':
     W = 50
     n = len(profit)
     print ("The maximum Profit gained is : ",knapSack(W, weight, profit, n))
-#o(n W)
+    
+#o(n W) Time Complexity
+
+
+#def knapsack(weights, values, capacity):
+#    n = len(values)
+#    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+#
+#    # Build table dp[][] in a bottom-up manner
+#    for i in range(1, n + 1):
+#        for w in range(capacity + 1):
+#            if weights[i - 1] <= w:
+#                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+#            else:
+#                dp[i][w] = dp[i - 1][w]
+#
+#    return dp[n][capacity]
+
+# Example usage
+#weights = [2, 3, 4, 5]
+#values = [3, 4, 5, 6]
+#capacity = 5
+#max_value = knapsack(weights, values, capacity)
+#print("Maximum value in knapsack:", max_value)
